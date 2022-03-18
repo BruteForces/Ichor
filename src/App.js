85a7fa9d8d4cd034da2e2f77a9b5c4d12c834136
aslamcodes <mohamedaslammn.2003@gmail.com>
2022-03-18 19:00:29 +0530

    Constants and Arrangements

--- src/App.js
+++ src/App.js
@@ -6,15 +6,24 @@ import {
   useAuthDispatchContext,
 } from "./context/auth/AuthContext";
 import {
+  CHAT_SCREEN,
   HOME_SCREEN,
   LOGIN_SCREEN,
   REGISTER_SCREEN,
+  TOP_DONORS_SCREEN,
 } from "./screens/constants";
-import { HomeScreen, LoginScreen, RegisterScreen } from "./screens";
+import {
+  HomeScreen,
+  LoginScreen,
+  RegisterScreen,
+  ChatScreen,
+  TopDonorsScreen,
+} from "./screens";
 import { NavigationContainer } from "@react-navigation/native";
 import { LOGIN_SUCCESS } from "./context/auth/constants";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-const Stack = createNativeStackNavigator();
+
+const AuthStack = createNativeStackNavigator();
 const MainTab = createBottomTabNavigator();
 const App = () => {
   const { user } = useAuthContext();
@@ -37,15 +46,14 @@ const App = () => {
           }}
         >
           <MainTab.Screen
-            name={HOME_SCREEN}
-            component={HomeScreen}
-            screenOptions={{
-              headerShown: false,
-            }}
+            name={TOP_DONORS_SCREEN}
+            component={TopDonorsScreen}
           />
+          <MainTab.Screen name={HOME_SCREEN} component={HomeScreen} />
+          <MainTab.Screen name={CHAT_SCREEN} component={ChatScreen} />
         </MainTab.Navigator>
       ) : (
-        <Stack.Navigator
+        <AuthStack.Navigator
           screenOptions={{
             headerShown: false,
           }}
@@ -54,9 +62,9 @@ const App = () => {
             name={GETTING_STARTED_SCREEN}
             component={GettingStarted}
           /> */}
-          <Stack.Screen name={REGISTER_SCREEN} component={RegisterScreen} />
-          <Stack.Screen name={LOGIN_SCREEN} component={LoginScreen} />
-        </Stack.Navigator>
+          <AuthStack.Screen name={REGISTER_SCREEN} component={RegisterScreen} />
+          <AuthStack.Screen name={LOGIN_SCREEN} component={LoginScreen} />
+        </AuthStack.Navigator>
       )}
     </NavigationContainer>
   );
--- src/screens/index.js
+++ src/screens/index.js
@@ -2,3 +2,5 @@ export { HomeScreen } from "./HomeScreen";
 export { LoginScreen } from "./LoginScreen";
 export { RegisterScreen } from "./RegisterScreen";
 export { GettingStarted } from "./GettingStartedScreen";
+export { TopDonorsScreen } from "./TopDonorsScreen";
+export { ChatScreen } from "./ChatScreen";
